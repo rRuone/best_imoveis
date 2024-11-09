68c1ead49f1f2e35654d16b53ef019906f9b2554
Guilherme Castro <guilherme.gcastro20@gmail.com>
2024-11-09 10:34:22 -0300

    css de Subtotal, Total

--- resources/views/livewire/finalizar-pedido.blade.php
+++ resources/views/livewire/finalizar-pedido.blade.php
@@ -1,12 +1,17 @@
-<div>
-    <h5 class="h5-header">Subtotal:</h5>
-    <p>R$ {{ number_format($subtotal, 2, ',', '.') }}</p>
+<div style="margin-bottom: 80px;">
+    <div  class="collection mb-0 grey lighten-5">
+         <h5 class="h5-header" style="font-size: 1rem;">Subtotal:</h5>
+        <p>R$ {{ number_format($subtotal, 2, ',', '.') }}</p>
+        <hr>
+        <h5 class="h5-header" style="font-size: 1rem;">Taxa de Entrega:</h5>
+        <p>R$ {{ number_format($taxaEntrega, 2, ',', '.') }}</p>
+        <hr>
+        <h5 class="h5-header">Total:</h5>
+        <p><strong>R$ {{ number_format($total, 2, ',', '.') }}</strong></p>
 
-    <h5 class="h5-header">Taxa de Entrega:</h5>
-    <p>R$ {{ number_format($taxaEntrega, 2, ',', '.') }}</p>
-
-    <h5 class="h5-header">Total:</h5>
-    <p><strong>R$ {{ number_format($total, 2, ',', '.') }}</strong></p>
+    </div>
+   
+ 
 
     <div class="footer-container" style="position: fixed; bottom: 0; left: 0; width: 100%; background-color: #F5F5F5; border-top: 1px solid #ddd; padding: 10px 0; text-align: center; display: flex; justify-content: center; align-items: center;">
         @if(session()->has('error'))
